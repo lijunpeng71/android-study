b473c8f428cf0d3750626cf2a2d5cd141165385d
lijunpeng71 <lijunpeng71@163.com>
2019-08-14 18:08:15 +0800

    [dev] init

--- app/src/main/java/com/cyinfo/android/study/MainActivity.java
+++ app/src/main/java/com/cyinfo/android/study/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cyinfo.android.study;
 
 import android.os.Bundle;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
